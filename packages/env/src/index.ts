571d8363a1c2fe0cb794f6735a2d7a76d9463961
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-05-21 23:03:38 +0530

    adding docker-compose file

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
-import type { SHA512_256 } from "bun";
 
 export const env = createEnv({
   server: {
